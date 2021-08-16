87dcc4792df9b2d588caf2c98d60ed01aaad69ed
Denis Bugreev <den9.rcq@gmail.com>
2021-08-16 14:50:24 +0300

    Update text element

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const element = <h1>Hello World меня зовут: Денис Бугреев</h1>
+const element = <h1>Hello World меня зовут: Денис Бугреев!</h1>
 
 ReactDOM.render(element, document.getElementById('root'));
